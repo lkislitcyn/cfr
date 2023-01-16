3b3adec70e0658705cb227af8e37d945c3a2eca6
lkislitcyn <kluchnik2004@mail.ru>
2023-01-16 22:34:26 +0800

    little correct

--- src/main/java/ru/burytia/cft/Main.java
+++ src/main/java/ru/burytia/cft/Main.java
@@ -2,54 +2,48 @@ package ru.burytia.cft;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-
-import static java.lang.System.arraycopy;
 
 public class Main {
     public static void main(String[] args) {
-
-
         ArrayList<Integer> arrayList = new ArrayList<>();
 
-        try (BufferedReader file1 = new BufferedReader(new FileReader("/home/lkislitcyn/workdir/1.txt"));
-             BufferedReader file2 = new BufferedReader(new FileReader("/home/lkislitcyn/workdir/2.txt"));
-             BufferedWriter file3 = new BufferedWriter(new FileWriter("/home/lkislitcyn/workdir/3.txt"))) {
+        try (
+                BufferedReader file1 = new BufferedReader(new FileReader("/home/lkislitcyn/workdir/1.txt"));
+                BufferedReader file2 = new BufferedReader(new FileReader("/home/lkislitcyn/workdir/2.txt"));
+                BufferedWriter file3 = new BufferedWriter(new FileWriter("/home/lkislitcyn/workdir/3.txt"))) {
 
-            String int_line1, int_line2;
+            String intLine1, intLine2;
 
-            int_line1 = file1.readLine();
-            int_line2 = file2.readLine();
-            while (int_line1 != null && int_line2 != null) {
+            intLine1 = file1.readLine();
+            intLine2 = file2.readLine();
+            while (intLine1 != null && intLine2 != null) {
 
-                if (Integer.parseInt(int_line1) < Integer.parseInt(int_line2))
-                {
-                    arrayList.add(Integer.parseInt(int_line1));
-                    file3.write(int_line1+"\n");
-                    int_line1=file1.readLine();
+                if (Integer.parseInt(intLine1) < Integer.parseInt(intLine2)) {
+                    arrayList.add(Integer.parseInt(intLine1));
+                    file3.write(intLine1 + "\n");
+                    intLine1 = file1.readLine();
                 } else {
-                    arrayList.add(Integer.parseInt(int_line2));
-                    file3.write(int_line2+"\n");
-                    int_line2=file2.readLine();
+                    arrayList.add(Integer.parseInt(intLine2));
+                    file3.write(intLine2 + "\n");
+                    intLine2 = file2.readLine();
                 }
             }
 
-            while (int_line1 != null )
-            {
-                arrayList.add(Integer.parseInt(int_line1));
-                file3.write(int_line1+"\n");
-                int_line1=file1.readLine();
+            while (intLine1 != null) {
+                arrayList.add(Integer.parseInt(intLine1));
+                file3.write(intLine1 + "\n");
+                intLine1 = file1.readLine();
             }
 
-            while (int_line2 != null )
-            {
-                arrayList.add(Integer.parseInt(int_line2));
-                file3.write(int_line2+"\n");
-                int_line2=file2.readLine();
+            while (intLine2 != null) {
+                arrayList.add(Integer.parseInt(intLine2));
+                file3.write(intLine2 + "\n");
+                intLine2 = file2.readLine();
             }
 
 
-        } catch (FileNotFoundException e) {
+        } catch (
+                FileNotFoundException e) {
             System.out.println(e.getMessage());
             //throw new RuntimeException(e);
 
@@ -62,3 +56,5 @@ public class Main {
 
     }
 }
+
+
