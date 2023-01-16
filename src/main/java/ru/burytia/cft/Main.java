5a8cda6eb0f210da2d00c3a25094a1958d783b7f
lkislitcyn <kluchnik2004@mail.ru>
2023-01-16 21:20:45 +0800

    change mas on arraylist

--- src/main/java/ru/burytia/cft/Main.java
+++ src/main/java/ru/burytia/cft/Main.java
@@ -1,6 +1,7 @@
 package ru.burytia.cft;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static java.lang.System.arraycopy;
@@ -8,45 +9,41 @@ import static java.lang.System.arraycopy;
 public class Main {
     public static void main(String[] args) {
 
-        int[] mas1 = new int[15], mas2 = new int[20];
-        int[] mas3 = new int[mas1.length + mas2.length];
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
 
         try (BufferedReader file1 = new BufferedReader(new FileReader("/home/lkislitcyn/workdir/1.txt"));
-             BufferedReader file2 = new BufferedReader(new FileReader("/home/lkislitcyn/workdir/2.txt"))) {
+             BufferedReader file2 = new BufferedReader(new FileReader("/home/lkislitcyn/workdir/2.txt"));
+             BufferedWriter file3 = new BufferedWriter(new FileWriter("/home/lkislitcyn/workdir/3.txt"))) {
 
             String int_line1, int_line2;
-            int i = 0, j = 0, k = 0;
+
             int_line1 = file1.readLine();
             int_line2 = file2.readLine();
             while (int_line1 != null && int_line2 != null) {
 
                 if (Integer.parseInt(int_line1) < Integer.parseInt(int_line2))
                 {
-                    mas3[k++]=Integer.parseInt(int_line1);
+                    arrayList.add(Integer.parseInt(int_line1));
                     int_line1=file1.readLine();
                 } else {
-                    mas3[k++]=Integer.parseInt(int_line2);
+                    arrayList.add(Integer.parseInt(int_line2));
                     int_line2=file2.readLine();
                 }
             }
 
             while (int_line1 != null )
             {
-                mas3[k++]=Integer.parseInt(int_line1);
+                arrayList.add(Integer.parseInt(int_line1));
                 int_line1=file1.readLine();
             }
 
             while (int_line2 != null )
             {
-                mas3[k++]=Integer.parseInt(int_line2);
+                arrayList.add(Integer.parseInt(int_line2));
                 int_line2=file2.readLine();
             }
 
-//            for(int i=0;(int_line = file1.readLine()) != null;i++)
-//
-//                mas1[i]=Integer.parseInt(int_line);
-//            for(int i=0;(int_line = file2.readLine()) != null;i++)
-//                mas2[i]=Integer.parseInt(int_line);
 
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
@@ -56,21 +53,8 @@ public class Main {
             System.out.println(e.getMessage());
             //throw new RuntimeException(e);
         }
-//        System.out.println(Arrays.toString(mas1));
-//        System.out.println(Arrays.toString(mas2));
 
-
-//        int i=0, j=0, k=0;
-//        while(i < mas1.length && j < mas2.length) {
-//            mas3[k++] = mas1[i] < mas2[j] ? mas1[i++] : mas2[j++];
-//        }
-//        if(i< mas1.length) {
-//            arraycopy(mas1, i, mas3, k, mas1.length - i);
-//        } else if(j< mas2.length) {
-//            arraycopy(mas2, j, mas3, k, mas2.length - j);
-//        }
-//
-        System.out.println(Arrays.toString(mas3));
+        System.out.println(arrayList);
 
     }
 }
