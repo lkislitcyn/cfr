432158e5dceb7d8cbab2e37b242074ed5e3b85fa
lkislitcyn <kluchnik2004@mail.ru>
2023-01-17 21:10:02 +0800

    make ascending, descending sort for both types

--- src/main/java/ru/burytia/cft/Main.java
+++ src/main/java/ru/burytia/cft/Main.java
@@ -3,17 +3,27 @@ package ru.burytia.cft;
 
 public class Main {
     public static void main(String[] args) {
-        String fileIn1="/home/lkislitcyn/workdir/integer/1.txt";
-        String fileIn2="/home/lkislitcyn/workdir/integer/2.txt";
-        String fileOut="/home/lkislitcyn/workdir/integer/3.txt";
+        String fileIn1="/home/lkislitcyn/workdir/intAscending/1.txt";
+        String fileIn2="/home/lkislitcyn/workdir/intAscending/2.txt";
+        String fileOut="/home/lkislitcyn/workdir/intAscending/3.txt";
         MergeSortFile mergeSortFile = new MergeSortFile();
-        mergeSortFile.margeSortIntFile(fileIn1,fileIn2,fileOut);
+        mergeSortFile.margeSortFileAsc(fileIn1,fileIn2,fileOut,0);
 
-        fileIn1="/home/lkislitcyn/workdir/string/1.txt";
-        fileIn2="/home/lkislitcyn/workdir/string/2.txt";
-        fileOut="/home/lkislitcyn/workdir/string/3.txt";
+        fileIn1="/home/lkislitcyn/workdir/strAscending/1.txt";
+        fileIn2="/home/lkislitcyn/workdir/strAscending/2.txt";
+        fileOut="/home/lkislitcyn/workdir/strAscending/3.txt";
 
-        mergeSortFile.margeSortStrFile(fileIn1,fileIn2,fileOut);
+        mergeSortFile.margeSortFileAsc(fileIn1,fileIn2,fileOut,1);
+
+        fileIn1="/home/lkislitcyn/workdir/intDescending/1.txt";
+        fileIn2="/home/lkislitcyn/workdir/intDescending/2.txt";
+        fileOut="/home/lkislitcyn/workdir/intDescending/3.txt";
+        mergeSortFile.margeSortFileDes(fileIn1,fileIn2,fileOut,0);
+
+        fileIn1="/home/lkislitcyn/workdir/strDescending/1.txt";
+        fileIn2="/home/lkislitcyn/workdir/strDescending/2.txt";
+        fileOut="/home/lkislitcyn/workdir/strDescending/3.txt";
+        mergeSortFile.margeSortFileDes(fileIn1,fileIn2,fileOut,1);
 
 
     }
--- src/main/java/ru/burytia/cft/MergeSortFile.java
+++ src/main/java/ru/burytia/cft/MergeSortFile.java
@@ -1,9 +1,75 @@
 package ru.burytia.cft;
 
 import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MergeSortFile {
-    void margeSortIntFile(String fileInWay1, String fileInWay2, String fileOutWay) {
+
+
+    void margeSortFileAsc(String fileInWay1, String fileInWay2, String fileOutWay, int type) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        try (
+                BufferedReader file1 = new BufferedReader(new FileReader(fileInWay1));
+                BufferedReader file2 = new BufferedReader(new FileReader(fileInWay2));
+                BufferedWriter file3 = new BufferedWriter(new FileWriter(fileOutWay))) {
+            String intLine1, intLine2;
+            intLine1 = file1.readLine();
+            intLine2 = file2.readLine();
+
+            switch (type) {
+                case 0: {
+                    while ((intLine1 != null) && (intLine2 != null))
+                        if (Integer.parseInt(intLine1) <= Integer.parseInt(intLine2)) {
+                            arrayList.add(Integer.parseInt(intLine1));
+                            file3.write(intLine1 + "\n");
+                            intLine1 = file1.readLine();
+                        } else {
+                            arrayList.add(Integer.parseInt(intLine2));
+                            file3.write(intLine2 + "\n");
+                            intLine2 = file2.readLine();
+                        }
+                    while (intLine1 != null || intLine2 != null) {
+                        if (intLine1 != null) {
+                            arrayList.add(Integer.parseInt(intLine1));
+                            file3.write(intLine1 + "\n");
+                            intLine1 = file1.readLine();
+                        }
+                        if (intLine2 != null) {
+                            arrayList.add(Integer.parseInt(intLine2));
+                            file3.write(intLine2 + "\n");
+                            intLine2 = file2.readLine();
+                        }
+                    }
+                }
+                case 1:
+                    while ((intLine1 != null) && (intLine2 != null))
+                        if (intLine2.compareTo(intLine1) > 0) {
+                            file3.write(intLine1 + "\n");
+                            intLine1 = file1.readLine();
+                        } else {
+                            file3.write(intLine2 + "\n");
+                            intLine2 = file2.readLine();
+                        }
+                    while (intLine1 != null || intLine2 != null) {
+                        if (intLine1 != null) {
+                            file3.write(intLine1 + "\n");
+                            intLine1 = file1.readLine();
+                        }
+                        if (intLine2 != null) {
+                            file3.write(intLine2 + "\n");
+                            intLine2 = file2.readLine();
+                        }
+                    }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(arrayList);
+    }
+
+    void margeSortFileDes(String fileInWay1, String fileInWay2, String fileOutWay, int type) {
 
         try (
                 BufferedReader file1 = new BufferedReader(new FileReader(fileInWay1));
@@ -12,60 +78,64 @@ public class MergeSortFile {
             String intLine1, intLine2;
             intLine1 = file1.readLine();
             intLine2 = file2.readLine();
-            while (intLine1 != null && intLine2 != null) {
+            switch (type) {
+                case 0:
+                    while ((intLine1 != null) && (intLine2 != null))
+                        if (Integer.parseInt(intLine1) >= Integer.parseInt(intLine2)) {
+                            file3.write(intLine1 + "\n");
+                            intLine1 = file1.readLine();
+                        } else {
+                            file3.write(intLine2 + "\n");
+                            intLine2 = file2.readLine();
+                        }
+                    while (intLine1 != null || intLine2 != null) {
+                        if (intLine1 != null) {
+                            file3.write(intLine1 + "\n");
+                            intLine1 = file1.readLine();
+                        }
+                        if (intLine2 != null) {
+                            file3.write(intLine2 + "\n");
+                            intLine2 = file2.readLine();
+                        }
+                    }
 
-                if (Integer.parseInt(intLine1) < Integer.parseInt(intLine2)) {
-                    file3.write(intLine1 + "\n");
-                    intLine1 = file1.readLine();
-                } else {
-                    file3.write(intLine2 + "\n");
-                    intLine2 = file2.readLine();
-                }
+                case 1:
+                    while ((intLine1 != null) && (intLine2 != null))
+                        if (intLine1.compareTo(intLine2) > 0) {
+                            file3.write(intLine1 + "\n");
+                            intLine1 = file1.readLine();
+                        } else {
+                            file3.write(intLine2 + "\n");
+                            intLine2 = file2.readLine();
+                        }
+                    while (intLine1 != null || intLine2 != null) {
+                        if (intLine1 != null) {
+                            file3.write(intLine1 + "\n");
+                            intLine1 = file1.readLine();
+                        }
+                        if (intLine2 != null) {
+                            file3.write(intLine2 + "\n");
+                            intLine2 = file2.readLine();
+                        }
+                    }
             }
-            mergeSortTail(file1, file2, file3, intLine1, intLine2);
         } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-
-    }
-
-    private void mergeSortTail(BufferedReader file1, BufferedReader file2, BufferedWriter file3, String intLine1, String intLine2) throws IOException {
-        while (intLine1 != null || intLine2 != null) {
-            if (intLine1 != null) {
-                file3.write(intLine1 + "\n");
-                intLine1 = file1.readLine();
-            }
-            if (intLine2 != null) {
-                file3.write(intLine2 + "\n");
-                intLine2 = file2.readLine();
-            }
+            throw new RuntimeException(e);
         }
     }
-
-    void margeSortStrFile(String fileInWay1, String fileInWay2, String fileOutWay) {
-        try (BufferedReader file1 = new BufferedReader(new FileReader(fileInWay1));
-             BufferedReader file2 = new BufferedReader(new FileReader(fileInWay2));
-             BufferedWriter file3 = new BufferedWriter(new FileWriter(fileOutWay))) {
-            String intLine1, intLine2;
-            intLine1 = file1.readLine();
-            intLine2 = file2.readLine();
-
-            while (intLine1 != null && intLine2 != null) {
-                if (intLine2.compareTo(intLine1) > 0) {
-                    file3.write(intLine1 + "\n");
-                    intLine1 = file1.readLine();
-                } else {
-                    file3.write(intLine2 + "\n");
-                    intLine2 = file2.readLine();
-                }
-            }
-            mergeSortTail(file1, file2, file3, intLine1, intLine2);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-
-    }
+//        private void mergeSortTail(BufferedReader file1, BufferedReader file2, BufferedWriter file3, String intLine1, String intLine2) throws IOException {
+//            while (intLine1 != null || intLine2 != null) {
+//            if (intLine1 != null) {
+//                arrayList.add(Integer.parseInt(intLine1));
+//                file3.write(intLine1 + "\n");
+//                intLine1 = file1.readLine();
+//            }
+//            if (intLine2 != null) {
+//                arrayList.add(Integer.parseInt(intLine2));
+//                file3.write(intLine2 + "\n");
+//                intLine2 = file2.readLine();
+//            }
+//        }
+//    }
 }
 
