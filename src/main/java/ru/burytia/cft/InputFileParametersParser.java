872b68196122d38004d85d59c1009bef6621cf13
lkislitcyn <kluchnik2004@mail.ru>
2023-01-22 13:57:57 +0800

    little change

--- src/main/java/ru/burytia/cft/InputFileParametersParser.java
+++ src/main/java/ru/burytia/cft/InputFileParametersParser.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class InputFileParametersParser {
     private final static String TEMP_FILE = "temp.txt";
@@ -13,18 +14,20 @@ public class InputFileParametersParser {
         var sortDirection = inputFileParameters.getSortDirection();
         var fileOut = inputFileParameters.getOutFile();
         var mergeSortFile = new MergeSortFile();
+        var tempFile = Paths.get(TEMP_FILE);
+        var fileOutPath = Paths.get(fileOut);
         if (inputFileParameters.getInFiles().size() > 1) {
             mergeSortFile.mergeSortFile(sortDirection, dataType, fileOut, inputFileParameters.getInFiles().get(0), inputFileParameters.getInFiles().get(1));
-            Path tempFile = Paths.get(TEMP_FILE);
+
             for (int i = 2; i < (inputFileParameters.getInFiles().size()); i++) {
-                Files.move(Paths.get(fileOut), tempFile);
+                Files.move(fileOutPath, tempFile);
                 mergeSortFile.mergeSortFile(sortDirection, dataType, fileOut, TEMP_FILE, inputFileParameters.getInFiles().get(i));
                 if (Files.exists(tempFile)) {
                     Files.delete(tempFile);
                 }
             }
         } else {
-            Files.move(Paths.get(inputFileParameters.getInFiles().get(0)), Paths.get(fileOut));
+            Files.copy(Paths.get(inputFileParameters.getInFiles().get(0)), fileOutPath, StandardCopyOption.REPLACE_EXISTING);
         }
     }
 }
