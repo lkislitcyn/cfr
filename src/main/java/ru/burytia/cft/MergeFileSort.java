4a87a4ba39a37830502218eda64e2dd3671ba2a8
lkislitcyn <kluchnik2004@mail.ru>
2023-02-15 23:01:22 +0800

    work with exceptions

--- src/main/java/ru/burytia/cft/MergeFileSort.java
+++ src/main/java/ru/burytia/cft/MergeFileSort.java
@@ -18,7 +18,11 @@ public class MergeFileSort {
                 mergeSort(strCompare(sortDirection), inReader1, inReader2, outWriter);
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+           e.printStackTrace();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            System.err.println("Symbol in file " + inFile1 + " or " + inFile2);
+            //e.printStackTrace();
         }
     }
 
@@ -47,20 +51,21 @@ public class MergeFileSort {
         mergeSortTail(line2, in2, out);
     }
 
-    Comparator<String> intCompare(SortDirection sortDirection) {
+    Comparator<String> intCompare(SortDirection sortDirection) throws NumberFormatException {
         return new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                try {
-                    if (sortDirection == SortDirection.ASC) {
-                        return Integer.parseInt(o1) - Integer.parseInt(o2);
-                    } else {
-                        return Integer.parseInt(o2) - Integer.parseInt(o1);
-                    }
-                } catch (NumberFormatException ex) {
-                    System.out.println("Symbol in array");
+                //try {
+                if (sortDirection == SortDirection.ASC) {
+                    return Integer.parseInt(o1) - Integer.parseInt(o2);
+                } else {
+                    return Integer.parseInt(o2) - Integer.parseInt(o1);
                 }
-                return 0;
+//                } catch (NumberFormatException ex) {
+//                    System.err.println("Symbol in input file");
+//                    ex.printStackTrace();
+//                }
+//                return 0;
             }
         };
     }
